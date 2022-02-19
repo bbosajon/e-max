363ee64e2c2f65b135e762b27ec82494a92bf9db
bbosajon <steinsjb@gmail.com>
2022-02-19 21:03:25 +0300

    allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) # True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['e-max.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
